09ac53d830788b2799a61e7431af08723009d8b3
Workshop Participant <participant@workshops.aws>
2025-08-13 05:36:48 +0000

    modify region

--- bin/demoawspipeline.ts
+++ bin/demoawspipeline.ts
@@ -3,4 +3,6 @@ import * as cdk from 'aws-cdk-lib';
 import { DemoawspipelineStack } from '../lib/demoawspipeline-stack';
 
 const app = new cdk.App();
-new DemoawspipelineStack(app, 'DemoawspipelineStack', {});
+new DemoawspipelineStack(app, 'DemoawspipelineStack', {
+  env: { account: "102132116425", region: "us-west-2" }
+});
